366db1c8c52114abd67d083c09eb84e224bed60d
Viocat <44540969+Viocat@users.noreply.github.com>
2022-12-20 01:49:41 +0200

    Add files via upload

---
+++ task1/js/dz10.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Переписать код ниже с использованием конструкции switch…case
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if(numOrStr === null) {
+//     console.log('вы отменили')
+// } else if( numOrStr.trim() === '' ) {
+//     console.log('Empty String');
+// } else if( isNaN( +numOrStr ) ) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (Number.isNaN(+numOrStr)) {
+    case true:
+        console.log(' number is Ba_NaN')
+        break;
+    case false:
+        switch (numOrStr.trim()) {
+            case null:
+                console.log('вы отменили');
+                break;
+            case '':
+                console.log('Empty String');
+                break;
+            default:
+                console.log('OK!');
+                console.log(numOrStr);
+                break;
+        }
+        default:
+            break;
+}
