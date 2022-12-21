cc54691901ff6967d9559421956aaccda69c9529
Viocat <44540969+Viocat@users.noreply.github.com>
2022-12-21 19:35:30 +0200

    Update dz12remake.js

--- task4/js/dz12remake.js
+++ task4/js/dz12remake.js
@@ -1,31 +1,4 @@
-// "use strict";
-// // // 1.
-// // // Вам даны две переменные x и y, менять их не нужно. При помощи тернарного оператора сравните их и если x больше, чем y, то выведите фразу: «x больше, чем y», иначе выведите фразу: «x не больше, чем y».
-// // // let x = 10, y = 7;
-
-// // let x=10;
-// // let y=7;
-// // x>y?console.log('x больше, чем y'):console.log('x не больше, чем y');
-
-// // // 2.
-// // // Пользователь вводит какое-то число (num). Используя конструкцию if..else, напишите код, который делает запрос: «Введите число».Если посетитель вводит четное число, то выводить «»Число » + num + » четное»», если нечетное: «Число » + num + » нечетное
-// // let num=+prompt('Введите число');
-// // if(num%2){
-// //     alert(`Число ${num} не четное`);
-// // }else{
-// //     alert(`Число ${num} четное`);
-// // }
-
-// // 3.
-// // Напишите код, который предлагает пользователю ввести целое число. Нужно вывести на экран сколько в этом числе цифр, а также положительное оно или отрицательное. Например, «Число » + num + » однозначное положительное». Достаточно будет определить, является ли число однозначным, двухзначным или трехзначным и более.
-
-// // Чтобы узнать длину строки которую ввел юзер в prompt нужно использовать
-
-// // let a = 'string'; 
-// // console.log(a.length)
-
-// let a = +prompt('Введите целое число').trim(); 
-// switch(Number.isInteger(a));
+"use strict";
 
 let birthYear = prompt(' enter number');
 let age=null;
@@ -131,54 +104,3 @@ switch(type){
 
 
 
-// switch(birthYear){
-//     case '':
-//      alert(`You haven't entered the year of your birth`);
-//      break;
-//     case null:
-//         alert(`You haven't entered the year of your birth`);
-//         break;
-//     default:
-//         switch(type){
-//             case false:
-            
-//                 age=new Date().getFullYear() - Number(birthYear);
-//                 // console.log('not nan');
-//                 //запускаем запрос города
-//                 city=prompt('enter city');
-//                 type = Number.isNaN(1 * city);
-//                 if(type===true){
-//                     if(city!==null && city!==''){
-//                         // console.log('City entered correctly. City = '+city);
-//                         switch(city){
-//                             case capitals[0]:
-//                                 country=countries[0];
-//                                 alert(country);
-//                                 break;
-//                             case capitals[1]:
-//                                 country=countries[1];
-//                                 break;
-//                             case capitals[2]:
-//                                 country=countries[2];
-//                                 break;
-//                             default:
-//                                 break;
-//                         }
-//                         // sports=prompt('Your favorite sports:\n Football\n Hockey\n Tennis');
-//                         // switch(sports){
-                            
-//                         // }
-//                     }else{
-//                         alert(`You haven't entered a city you live in`);
-//                     }
-//                 }else{
-//                     alert('Incorrect City name. goodbye')
-//                 }
-
-//                 break;
-//             default:
-//                 // 
-//                 break;
-//         }
-
-// }
